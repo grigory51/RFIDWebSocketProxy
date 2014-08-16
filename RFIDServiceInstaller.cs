d38617643203de8bebc449f76611ee35e3646c1f
Grigory Ozhegov <grigory@ozhegov.name>
2014-08-16 15:54:45 +0400

    установщик сервиса

---
+++ RFIDServiceInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+namespace RFIDWSProxy {
+    [RunInstaller(true)]
+    public class RFIDServiceInstaller : Installer {
+        public RFIDServiceInstaller() {
+            ServiceProcessInstaller processInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "RFIDWSProxy";
+            serviceInstaller.DisplayName = "RFIDWSProxy";
+         
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
