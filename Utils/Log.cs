e8fadfd618e62cb0b21fdcf79a575531f7a5b40c
Grigory Ozhegov <grigory@ozhegov.name>
2014-08-16 15:55:05 +0400

    лог в зависимости от среды выполнения

---
+++ Utils/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace RFIDWSProxy.Utils {
+    class Log {
+        protected static EventLog eventLog = new System.Diagnostics.EventLog();
+
+        public Log() {
+            ((System.ComponentModel.ISupportInitialize)(eventLog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(eventLog)).EndInit();
+        }
+
+        public static void Write(string msg) {
+            if (Environment.UserInteractive) {
+                Console.WriteLine(msg);
+            } else {
+                try {
+                    string serviceName = "RFIDWSProxy";
+                    if (!EventLog.SourceExists(serviceName)) {
+                        EventLog.CreateEventSource(serviceName, serviceName);
+                    }
+                    eventLog.Source = serviceName;
+                    eventLog.WriteEntry(msg);
+                } catch { }
+            }
+        }
+    }
+}
